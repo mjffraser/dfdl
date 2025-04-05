7f75c0174df588d3110c4b742f43861ca6cbe7bd
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2025-04-05 05:37:17 -0600

    set up client cpp files

---
+++ src/client/internal/clientConfigs.cpp
@@ -0,0 +1,15 @@
+#include "client/internal/clientConfigs.hpp"
+namespace dfd
+{
+    int getHostListFromDisk(std::vector<SourceInfo> &dest, const std::string &host_path)
+    {
+    }
+
+    int storeHostListToDisk(const std::vector<SourceInfo> &hosts, const std::string &host_path)
+    {
+    }
+
+    uint64_t getMyUUID(const std::string &uuid_path)
+    {
+    }
+}
---
+++ src/client/internal/peerRequests.cpp
@@ -0,0 +1,11 @@
+#include "client/internal/peerRequests.hpp"
+namespace dfd
+{
+    void seedToPeer(std::atomic<bool> &shutdown, int peer_sock)
+    {
+    }
+
+    void clientListener(std::atomic<bool> &shutdown)
+    {
+    }
+}
