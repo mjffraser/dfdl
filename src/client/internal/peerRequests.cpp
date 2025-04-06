cbf9f2f7d460481e0daefc12c5823193f5901d01
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2025-04-06 02:03:50 -0600

    temp: seedToPeer partial impl.

--- src/client/internal/peerRequests.cpp
+++ src/client/internal/peerRequests.cpp
@@ -13,6 +13,18 @@ namespace dfd
 {
     void seedToPeer(std::atomic<bool> &shutdown, int peer_sock)
     {
+        while (!shutdown.load())
+        {
+            // recieve client download init request
+            std::vector<uint8_t> buffer;
+            if (!recvOkay(peer_sock, buffer, DOWNLOAD_INIT))
+                return;
+
+            auto [uuid, c_size] = parseDownloadInit(buffer);
+            // TO-DO: rest of implementation
+        }
+
+        closeSocket(peer_sock); // Clean up the socket when done
     }
 
     void clientListener(std::atomic<bool> &shutdown, std::atomic<uint16_t> &port)
